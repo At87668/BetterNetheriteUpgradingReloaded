d7fe6c0c1e5f85a56df95b7f27a26e6904252691
Author87668 <105369287+At87668@users.noreply.github.com>
2024-09-22 16:53:34 +0800

    1.2 Update

--- src/main/java/link/star_dust/BetterNetheriteUpgradingReloaded/Main.java
+++ src/main/java/link/star_dust/BetterNetheriteUpgradingReloaded/Main.java
@@ -1,14 +1,19 @@
 package link.star_dust.BetterNetheriteUpgrading;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new ForgeListener(this), this);
+        getCommand("bnu").setExecutor(this);
         getLogger().info("BetterNetheriteUpgrading Reloaded Version now enabled!");
     }
 
@@ -19,5 +24,22 @@ public class Main extends JavaPlugin {
 
     public FileConfiguration getPluginConfig() {
         return this.getConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("betternetheriteupgrading") && args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (!player.hasPermission("betternetheriteupgrading.reload")) {
+                    player.sendMessage("§cYou do not have permission to use this command.");
+                    return true;
+                }
+            }
+            reloadConfig();
+            sender.sendMessage("§aBetterNetheriteUpgrading Reloaded Version config reloaded.");
+            return true;
+        }
+        return false;
     }
 }
